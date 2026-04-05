385c2f126c903479da23f87eeddccda8d43a6e43
RAAWAN- <255635953+SILENT-HUMAN0@users.noreply.github.com>
2026-04-05 06:51:46 +0545

    update menu routes file

--- backend/src/routes/restaurantRoutes.js
+++ backend/src/routes/restaurantRoutes.js
@@ -1,12 +1,7 @@
-import express from "express";
-import {
-  listRestaurants,
-  getRestaurantById,
-} from "../controllers/restaurantController.js";
+import { Router } from "express";
+import { getRestaurants, getRestaurantById } from "../controllers/restaurantController.js";
 
-const router = express.Router();
-
-router.get("/", listRestaurants);
+const router = Router();
+router.get("/", getRestaurants);
 router.get("/:id", getRestaurantById);
-
 export default router;
