0ae0dd393fc7aac4751946ecd3677bf38566094f
RAAWAN- <255635953+SILENT-HUMAN0@users.noreply.github.com>
2026-04-10 18:22:28 +0545

    update restaurant routes v7

--- backend/src/routes/restaurantRoutes.js
+++ backend/src/routes/restaurantRoutes.js
@@ -1,22 +1,10 @@
 import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
-import { allowRoles } from "../middleware/roleMiddleware.js";
-import { ROLES } from "../constants/roles.js";
-import {
-  createMenuItem,
-  updateMenuItem,
-  deleteMenuItem,
-} from "../controllers/menuController.js";
+import { listRestaurants, getRestaurantById, getRestaurantFilters, getRestaurantSections } from "../controllers/restaurantController.js";
 
 const router = express.Router();
-
-router.post("/", protect, allowRoles(ROLES.RESTAURANT_ADMIN), createMenuItem);
-router.put("/:id", protect, allowRoles(ROLES.RESTAURANT_ADMIN), updateMenuItem);
-router.delete(
-  "/:id",
-  protect,
-  allowRoles(ROLES.RESTAURANT_ADMIN),
-  deleteMenuItem,
-);
+router.get("/", listRestaurants);
+router.get("/filters", getRestaurantFilters);
+router.get("/sections", getRestaurantSections);
+router.get("/:id", getRestaurantById);
 
 export default router;
