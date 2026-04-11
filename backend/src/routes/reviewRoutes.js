6305cc22a1fc6e46bad5d572a8d330effabb916b
ssamvid <samvidstha25@gmail.com>
2026-04-11 10:15:15 +0545

    Reviews Routes v8

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -5,11 +5,15 @@ import { ROLES } from "../constants/roles.js";
 import {
   createReview,
   getRestaurantReviews,
+  getMyReviews,
+  getMyReviewableItems,
 } from "../controllers/reviewController.js";
 
 const router = express.Router();
 
 router.get("/restaurant/:restaurantId", getRestaurantReviews);
+router.get("/mine", protect, allowRoles(ROLES.CUSTOMER), getMyReviews);
+router.get("/reviewable", protect, allowRoles(ROLES.CUSTOMER), getMyReviewableItems);
 router.post("/", protect, allowRoles(ROLES.CUSTOMER), createReview);
 
 export default router;
