37514effadebf8e94116e6cfdc58dc01a974897e
aayushmhzn555-collab <aayushmhzn555@gmail.com>
2026-04-11 12:08:10 +0545

    Added SignupPage.jsx in views folder v8

--- frontend/src/views/SignupPage.jsx
+++ frontend/src/views/SignupPage.jsx
@@ -1,7 +1,1 @@
-import { useNavigate } from "react-router-dom";
-import { LoginPage } from "./LoginPage";
-
-export function SignupPage() {
-  // Redirect to login page with signup tab active
-  return <LoginPage />;
-}
+export { LoginPage as SignupPage } from './LoginPage';
