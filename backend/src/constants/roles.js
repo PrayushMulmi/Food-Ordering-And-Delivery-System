c1c2ca9d84a3b7ae9e0cf596d12508211499c6c7
ssamvid <samvidstha25@gmail.com>
2026-04-11 10:05:45 +0545

    Fixed roles issue

--- backend/src/constants/roles.js
+++ backend/src/constants/roles.js
@@ -1,6 +1,5 @@
 export const ROLES = {
-    CUSTOMER: "customer",
-    RESTAURANT_ADMIN: "restaurant_admin",
-    SUPER_ADMIN: "super_admin",
-  };
-  
+  CUSTOMER: "customer",
+  RESTAURANT_ADMIN: "restaurant_admin",
+  SUPER_ADMIN: "super_admin",
+};
