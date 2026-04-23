6881ea1eb67be1da718998d00338a686b89f2938
RAAWAN- <255635953+SILENT-HUMAN0@users.noreply.github.com>
2026-04-23 08:15:13 +0545

    Solved merge conflict

--- backend/src/routes/restaurantRoutes.js
+++ backend/src/routes/restaurantRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import { listRestaurants, getRestaurantById, getRestaurantFilters, getRestaurantSections } from "../controllers/restaurantController.js";
 
@@ -7,13 +6,11 @@ router.get("/", listRestaurants);
 router.get("/filters", getRestaurantFilters);
 router.get("/sections", getRestaurantSections);
 router.get("/:id", getRestaurantById);
-
-=======
 import { Router } from "express";
 import { getRestaurants, getRestaurantById } from "../controllers/restaurantController.js";
 
 const router = Router();
 router.get("/", getRestaurants);
 router.get("/:id", getRestaurantById);
->>>>>>> origin/main
+
 export default router;
