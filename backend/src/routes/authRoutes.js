aabcddb1ea80e6128f60ff36fd8d2631c4f06d63
ssamvid <samvidstha25@gmail.com>
2026-05-01 21:09:56 +0545

    Auth Routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -9,6 +9,7 @@ import {
   loginValidation,
 } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { listMySavedLocations, createMySavedLocation, updateMySavedLocation, deleteMySavedLocation } from "../controllers/savedLocationController.js";
 
 const router = express.Router();
 
@@ -17,5 +18,9 @@ router.post("/login", loginValidation, login);
 router.get("/me", protect, getMe);
 router.put("/me", protect, updateMyProfile);
 router.put("/change-password", protect, changeMyPassword);
+router.get("/me/locations", protect, listMySavedLocations);
+router.post("/me/locations", protect, createMySavedLocation);
+router.put("/me/locations/:id", protect, updateMySavedLocation);
+router.delete("/me/locations/:id", protect, deleteMySavedLocation);
 
 export default router;
