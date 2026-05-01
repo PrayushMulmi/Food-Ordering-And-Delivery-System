2c1a80bebb926d492fded22ff2e8a5161196dc5b
aayushmhzn555-collab <aayushmhzn555@gmail.com>
2026-05-01 17:21:42 +0545

    Added main.jsx in src folder v11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { jsx } from "react/jsx-runtime";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/index.css";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsx(App, {}));
