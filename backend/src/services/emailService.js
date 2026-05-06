f92cdc2faf4e1982eec22adbf5a0f816e39c8524
ssamvid <samvidstha25@gmail.com>
2026-05-06 22:20:48 +0545

    Fixed mock email

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,5 +1,4 @@
 export const sendEmail = async ({ to, subject, html }) => {
-    console.log("Mock email sent:", { to, subject, html });
-    return true;
-  };
-  
+  console.log("Mock email sent:", { to, subject, html });
+  return true;
+};
