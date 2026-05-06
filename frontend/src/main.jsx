84f8e8f54dadfc903c9acbc545b40f34d154a030
aayushmhzn555-collab <aayushmhzn555@gmail.com>
2026-05-06 23:21:19 +0545

    Added main.jsx FINAL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,7 @@ import { jsx } from "react/jsx-runtime";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
+import { initTheme } from "./lib/theme";
+
+initTheme();
 createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsx(App, {}));
