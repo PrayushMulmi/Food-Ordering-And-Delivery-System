d7a859a4c93562f0d3cfe128e0026c5e534d7434
samvidstha <samvidstha25@gmail.com>
2026-05-18 18:37:46 +0545

    Final adjustment to the folders

--- backend/src/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createOrder, getOrders } from "../controllers/orderController.js";
-
-const router = Router();
-router.get("/", getOrders);
-router.post("/", createOrder);
-export default router;
