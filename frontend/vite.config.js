2e27f8dc963002b1ae0c2aebb93d61a3f5ee48e8
RAAWAN- <255635953+SILENT-HUMAN0@users.noreply.github.com>
2026-05-19 10:48:44 +0545

    Configure Vite build setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  assetsInclude: ["**/*.svg", "**/*.csv"],
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+
+    allowedHosts: true,
+
+    proxy: {
+      "/api": {
+        target: "http://localhost:5050",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/uploads": {
+        target: "http://localhost:5050",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
