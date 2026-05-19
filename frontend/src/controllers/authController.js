c7435a8a1b1c2ae365823e5dd1a56a95f0533a6e
aayushmhzn555-collab <aayushmhzn555@gmail.com>
2026-05-19 11:42:12 +0545

    Fixed bugs

--- frontend/src/controllers/authController.js
+++
@@ -1,4 +0,0 @@
-import { loginRequest, registerRequest } from '../lib/api';
-
-export const authenticateUser = async (payload) => loginRequest(payload);
-export const registerUser = async (payload) => registerRequest(payload);
